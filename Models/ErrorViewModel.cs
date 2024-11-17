6d5399e8c7c181f1726073cad947f5eb62fccd5b
harrynguyen0710 <nnhoang0710@gmail.com>
2024-11-17 12:26:45 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace siu_smart_printing_service.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
