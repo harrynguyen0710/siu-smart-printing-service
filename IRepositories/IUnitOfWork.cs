bf4dc17bcffb82e58af9caf7c72858c4fae7e0a2
harrynguyen0710 <nnhoang0710@gmail.com>
2024-12-08 19:01:44 +0700

    feat: init design pattern for the entire project

---
+++ IRepositories/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace siu_smart_printing_service.IRepositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<TEntity> Repository<TEntity>() where TEntity : class;
+        Task<int> CompleteAsync();
+    }
+}
---
+++ Repositories/Repository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using siu_smart_printing_service.Data;
+using siu_smart_printing_service.IRepositories;
+
+namespace siu_smart_printing_service.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly ApplicationDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Add(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
